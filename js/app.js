e2ee9580ffdf7334c2c56a85bdc3f778fc5b4a86
Alan Palazzolo <apalazzolo@minnpost.com>
2014-02-04 21:32:19 -0600

    More goodness.

--- js/app.js
+++ js/app.js
@@ -8,13 +8,16 @@
   $(document).ready(function() {
 
     var mapSite = L.mapbox.map('mapSite', 'zzolo.gmlc0583', {
-      scrollWheelZoom: false,
-      zoomControl: false
+      scrollWheelZoom: false
     }).setView([45.87358, -93.77508], 18);
 
     var mapExtra = L.mapbox.map('mapExtras', 'zzolo.gmlj8b7g', {
       scrollWheelZoom: false
-    }).setView([45.493, -93.340], 8);
+    }).setView([45.912, -93.717], 9);
+
+    var mapExtraCities = L.mapbox.map('mapExtras-cities', 'zzolo.h6hmn0im', {
+      scrollWheelZoom: false
+    }).setView([44.96, -93.158], 11);
 
   });
 
