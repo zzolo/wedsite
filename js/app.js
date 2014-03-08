ee9ae1f945fa5f4671580f5aeb1d21016544d6ca
Alan Palazzolo <apalazzolo@minnpost.com>
2014-03-08 16:55:47 -0600

    Adding rsvp page.

--- js/app.js
+++ js/app.js
@@ -7,17 +7,19 @@
 
   $(document).ready(function() {
 
-    var mapSite = L.mapbox.map('mapSite', 'zzolo.gmlc0583', {
-      scrollWheelZoom: false
-    }).setView([45.87358, -93.77508], 18);
+    if ($('#mapSite').size() > 0) {
+      var mapSite = L.mapbox.map('mapSite', 'zzolo.gmlc0583', {
+        scrollWheelZoom: false
+      }).setView([45.87358, -93.77508], 18);
 
-    var mapExtra = L.mapbox.map('mapExtras', 'zzolo.gmlj8b7g', {
-      scrollWheelZoom: false
-    }).setView([45.912, -93.717], 9);
+      var mapExtra = L.mapbox.map('mapExtras', 'zzolo.gmlj8b7g', {
+        scrollWheelZoom: false
+      }).setView([45.912, -93.717], 9);
 
-    var mapExtraCities = L.mapbox.map('mapExtras-cities', 'zzolo.h6hmn0im', {
-      scrollWheelZoom: false
-    }).setView([44.96, -93.158], 11);
+      var mapExtraCities = L.mapbox.map('mapExtras-cities', 'zzolo.h6hmn0im', {
+        scrollWheelZoom: false
+      }).setView([44.96, -93.158], 11);
+    }
 
   });
 
